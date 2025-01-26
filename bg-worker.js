20f77b61cf9fa19bdab3c661acf9bbb7267636b8
TufanTFN <34925519+TufanTFN@users.noreply.github.com>
2025-01-26 10:46:57 +0100

    Add files via upload

---
+++ bg-worker.js
@@ -0,0 +1,80 @@
+// Code for background worker.
+// Its purpose is to listen to piping-server for form data and post to Telegram when received, i.e. relaying.
+// Polling piping-server API is achieved using setTimeout for efficiency. One relay seeds the next before returning.
+// 'main' in the following refers to "server.js".
+
+function urlEncoded2Json(str){
+    const arr = str.split('&');
+
+    const obj = new Object();
+
+    for (const el of arr) {
+        let elArray = el.split('=');
+        let val = decodeURIComponent(elArray[1].replace( /\+/g, ' ' )).replace(/"/g,'\\"'); // Decoded and escaped
+        obj[elArray[0]]=val;
+    }
+    
+    return JSON.stringify(obj);
+}
+
+async function pollApi(getFrom, postTo, TGchatID) {
+
+    const relay = async () => {
+        let data; // This will hold the received URL encoded form data
+        let errLvl = 2; // Default error level when errors are caught. May be overriden before using `throw`.
+        
+        // Listen to piping-server
+        try {
+            const response = await fetch(getFrom); // Make request
+
+            if (! response.ok) {
+                errLvl = 1; // Set error to critical/fatal
+                throw `GET @ ${getFrom} status: ${response.status}`;
+            }
+
+            data = urlEncoded2Json(await response.text());
+            // Send URL decoded form data as JSON string to main for logging. Also pass an error level.
+            postMessage([data, 0]);
+            
+        } catch (error) {
+            console.error(`${Date()}: Error making GET request -- ${error}`);
+            // Send error to main for logging. Error level: 1 for high priority / fatal.
+            postMessage(['Failed to fetch form data.', errLvl]);
+            return;
+
+        } finally {
+            setTimeout(relay, 0); // Schedule next relay
+        }
+
+        // POST to Telegram
+        let payload = {chat_id: TGchatID, text: data}; // conforming to Telegram API schema
+
+        try {
+            const response = await fetch(postTo, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'}, 
+                body: JSON.stringify(payload)
+            })
+            
+            if (! response.ok) {
+                throw `POST @ ${postTo} status: ${response.status}. Is chat ID = ${TGchatID} ok?`;
+            }
+
+        } catch (error) {        
+            console.error(`${Date()}: Error making POST request -- ${error}`);
+            // Send error to main for logging. Error level: 2 for low priority / non-fatal.
+            postMessage(['Failed to post form data to Telegram.', errLvl]);
+            return;
+        }
+
+        console.log(`${Date()}: Relay complete.`);
+    };
+
+    relay(); // Start the first relay
+}
+
+// Register pollApi has handler for the event of receiving any message from main
+onmessage = (e) => {
+  console.log("Message received from main script: " + JSON.stringify(e.data));
+  pollApi(e.data[0], e.data[1], e.data[2]);
+};
---
+++ server.js
@@ -0,0 +1,196 @@
+// Main entry point for the server. Deploys background worker in "bg-worker.js" for handling networking.
+
+let myWorker = null;
+let numReadMsgs = 0;
+let numTotalMsgs = 0;
+const logs = document.getElementById("logs");
+const toggleServer = document.getElementById("toggleServer");
+
+function logThis(report) {
+    const row = document.createElement("p");
+    row.append(`${Date()}: ${report}`);
+    logs.prepend(row);
+}
+
+// Handler for updating the display of number of unread messages
+function updateUnreadCount(){
+    if (spaCurrentPageID === "inbox") {
+        numReadMsgs = numTotalMsgs;
+    }
+    document.getElementById("unread").innerText = numTotalMsgs - numReadMsgs;
+}
+
+function inbox(json){
+    const data = JSON.parse(json); // Read form data into entry object
+    
+    if (data.From === "EasyFormViewCounter") {
+        let viewCount = localStorage.getItem("EasyFormViewCounter");
+        ++viewCount;
+        document.getElementById("EasyFormViewCounter").innerText = `which has ${viewCount} views`;
+        localStorage.setItem("EasyFormViewCounter", viewCount);
+        return;
+    }
+    
+    data.Timestamp = Date();
+
+    // Create table row:
+    const row = document.createElement("tr");
+
+    const header = document.getElementById("inboxHeader");
+    if (! numTotalMsgs) { header.replaceChildren();}
+    
+    for (const key in data) {
+        // Create cell:
+        const cell = document.createElement("td");
+
+        // Create a text entry:
+        entry = data[key];
+
+        // Append entry to cell:
+        cell.append(entry);
+
+        // Append cell to row:
+        row.append(cell);        
+
+        if (! numTotalMsgs) {
+            // Setup header according to the form fields. This is necessary as users may have custom form fields.
+            // Create header block:
+            const header_block = document.createElement("th");
+            header_block.append(key);
+            header.append(header_block);
+        }
+    }
+    
+    // Append row to table body:
+    document.getElementById("inboxTable").prepend(row);
+    
+    // Update number of total messages
+    ++numTotalMsgs;
+    updateUnreadCount("new");
+}
+
+function genUUID() {
+    // v4 UUID looks like xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx in hexadecimal. See Wikipedia.
+    // M stores version & N, the variant. All the x digits above are cryptographically random.
+    // For our uuid we simply choose the first block of hex chars from a v4 UUID.
+    document.getElementById("uuid").value = crypto.randomUUID().split('-')[0];
+}
+
+const fetchChatID = async () => {
+    logThis("Fetching Telegram chat ID");
+    const apiEndpoint = 'https://api.telegram.org/bot' + document.getElementById("TGbotKey").value + '/getUpdates';
+    const response = await fetch(apiEndpoint); // Make request
+    if (! response.ok) {
+        logThis(`Telegram API status code: ${response.status}. Is Bot API Token ok?`);
+        alert("Failed to fetch chat ID. Check your Bot API Token!");
+        return;
+    }
+    const data = await response.json();
+    try {
+        const TGchatID = data.result[0].message.chat.id;
+        document.getElementById("chatID").value = TGchatID;
+        localStorage.setItem("TGchatID", TGchatID);
+    } catch (e) {
+        alert("Failed to fetch chat ID. Send any text to the Telegram Bot then try again.");
+    }
+}
+
+function config() {
+    const relayList = ["https://ppng.io", "https://piping.glitch.me", "https://demo.httprelay.io/link"];
+    const uuid = document.getElementById("uuid").value;
+    // Choose a random index in [0, relayList.length]. Use first two nibbles of uuid as random number in range [0,256].
+    const randomIdx = Math.floor(parseInt(uuid.substr(0,2),16)*relayList.length/256);
+    const getFrom = relayList[randomIdx] + '/' + uuid;
+    localStorage.setItem("getFrom", getFrom);
+    const postTo = 'https://api.telegram.org/bot' + document.getElementById("TGbotKey").value + '/sendMessage';
+    localStorage.setItem("postTo", postTo);
+    spaHide("login");
+    spaGoTo("server");
+    localStorage.setItem("loggedIn", "true");
+}
+
+function startWorker() {
+    if (myWorker) {
+        return;
+    } else {
+        sessionStorage.setItem("server", "live");
+    }
+    
+    myWorker = new Worker("bg-worker.js");
+
+    // Register handler for messages from the background worker
+    myWorker.onmessage = (e) => {
+        const errLvl = e.data[1];
+        const msg = e.data[0];
+        if (! errLvl) {
+            inbox(msg);
+            logThis(`RECEIVED: ${msg}`);
+        } else if (errLvl === 1) {
+            stopWorker();
+            logThis(`FATAL ERROR: ${msg}. See console for details.`);
+            alert('Server stopped due to some critical error');
+        } else {
+            logThis(`ERROR: ${msg}. See console for details.`);
+        }
+    }
+
+    const getFrom = localStorage.getItem("getFrom");
+
+    // Communicate key data to the background worker
+    myWorker.postMessage([getFrom, localStorage.getItem("postTo"), localStorage.getItem("TGchatID")]);
+
+    toggleServer.value = "Kill Server";
+    toggleServer.disabled = false;
+
+    logThis("Server started");
+    document.getElementById("serverStatus").innerHTML = 'Live  <span class="spinner-grow spinner-grow-sm"></span>';
+    
+    document.getElementById("formActionURL").innerHTML = `<p class="alert alert-success">HTML Form Action URL: <u>${getFrom}</u></p>`;
+    document.getElementById("readyForm").href = `./${btoa(getFrom).replace(/\+/g,'_').replace(/\//g,'-').replace(/=+$/,'')}`;
+    document.getElementById("testFormBtn").setAttribute("formaction", getFrom);
+    spaShow("testForm");
+}
+
+function stopWorker() {
+    if (! myWorker) {
+        return;
+    }
+    myWorker.terminate();
+    myWorker = null;
+    sessionStorage.removeItem("server");
+    console.log("Worker terminated");
+    toggleServer.value = "Launch Server";
+    logThis("Server stopped");
+    document.getElementById("serverStatus").innerText = "Killed";
+}
+
+function toggleWorker() {
+    if (myWorker != null) {
+        stopWorker();
+    } else {
+        startWorker();
+    }
+}
+
+function signout() {
+    stopWorker();
+    localStorage.clear();
+    location.reload();
+}
+
+function main() {
+    // Enable config if no prior settings found in localStorage
+    if (localStorage.getItem("loggedIn")) {
+        spaHide("login");
+        startWorker();
+        spaGoTo("server");
+    } else {
+        spaGoTo("setup");
+    }
+
+}
+
+if (sessionStorage.getItem("server")) {
+    spaHide("login");
+    startWorker();
+}
